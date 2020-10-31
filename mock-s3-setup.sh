c43ff701a4bcddf2eb6a9851091a65fa80d96129
yihaoye <yihao.ye@vendhq.com>
2020-11-01 10:18:18 +1300

    mock s3 script

---
+++ mock-s3-setup.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# brew update
+# brew install ruby
+# # If you use bash
+# echo 'export PATH=/usr/local/Cellar/ruby/2.4.1_1/bin:$PATH' >> ~/.bash_profile 
+# # If you use ZSH:
+# echo 'export PATH=/usr/local/Cellar/ruby/2.4.1_1/bin:$PATH' >> ~/.zprofile
+gem install fakes3
--- mock-s3-up.sh
+++ mock-s3-up.sh
@@ -1,2 +1,4 @@
-gem install fakes3
+#!/usr/bin/env bash
+
+# run mock-s3-setup first before this script
 fakes3 -r ./mock-s3 -p 4567 --license 2909469511 # license number valid for 1 year, if expired, just apply for a new one for free
